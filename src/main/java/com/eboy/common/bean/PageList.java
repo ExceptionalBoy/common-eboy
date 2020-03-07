9cea6ace33c1a5bcbe67e0abf22ee8b9097020fd
王晓杰 <wangxiaojie@huimin100.cn>
2020-03-07 19:54:28 +0800

    我是谁，我在哪

--- src/main/java/com/eboy/common/bean/PageList.java
+++ src/main/java/com/eboy/common/bean/PageList.java
@@ -2,8 +2,8 @@ package com.eboy.common.bean;
 
 import com.alibaba.fastjson.JSONObject;
 
+import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
 
 /**
  * @ClassName PageList
@@ -12,41 +12,26 @@ import java.util.Collection;
  * @CreateTime 2020-03-07 18:57
  * @Version 1.0
  **/
-public class PageList<E>  extends ArrayList<E> {
+public class PageList<E> implements Serializable {
 
     private PageInfo pageInfo;
 
+    private ArrayList<E> list;
+
     public PageList(){
 
-    }
-
-    public PageList(int initialCapacity, PageInfo pageInfo) {
-        super(initialCapacity);
-        this.pageInfo = pageInfo;
     }
 
     public PageList(PageInfo pageInfo) {
         this.pageInfo = pageInfo;
     }
 
-    public PageList(Collection<? extends E> c, PageInfo pageInfo) {
-        super(c);
+    public PageList(ArrayList<E> list) {
+        this.list = list;
+    }
+
+    public PageList(PageInfo pageInfo, ArrayList<E> list) {
         this.pageInfo = pageInfo;
-    }
-
-    public PageInfo getPageInfo() {
-        return pageInfo;
-    }
-
-    public void setPageInfo(PageInfo pageInfo) {
-        this.pageInfo = pageInfo;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-                "\"pageInfo\":" + JSONObject.toJSONString(this.pageInfo) +
-                ", \"list\":" + JSONObject.toJSONString(this) +
-                "}";
+        this.list = list;
     }
 }
